7a76d62c34552487d77e6534e1883579b484799a
Yuidyy555 <yniioka@netprotections.co.jp>
2025-10-18 21:40:38 +0900

    ユーザー認証機能を強化し、ユーザー名とアバターの追加、パスワードリセットや確認メールのテンプレートを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+  validates :name, length: { maximum: 50 }, allow_blank: true
 end
